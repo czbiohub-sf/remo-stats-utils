75a888a8f3ac635e595f8c9c84470cf7af7b5def
Michelle Khoo <michelle.khoo.wl@gmail.com>
2024-01-31 14:30:46 -0800

    Add setup.py and upate README

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Oct 1, 2021
+
+@author: mwlkhoo
+"""
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="stats_utils",
+    version="0.0.1",
+    description="Statistics utilities for remoscope and corresponding paper",
+    long_description=readme(),
+    url="https://github.com/czbiohub-sf/remo-stats-utils",
+    author="Bioengineering | CZ Biohub SF",
+    author_email="michelle.khoo@czbiohub.org",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+	"yogo @ git+https://github.com/czbiohub-sf/yogo@master",
+    ],
+    classifiers=["CZ Biohub :: Bioengineering"],
+)
+
