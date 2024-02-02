0a21da66034371ccbdea4ee5bd8ce830e094b0ba
Michelle Khoo <michelle.khoo.wl@gmail.com>
2024-02-01 17:51:40 -0800

    Finished ABC

--- stats_utils/correct_counts.py
+++ stats_utils/correct_counts.py
@@ -7,7 +7,7 @@ import numpy.typing as npt
 from typing import List, Tuple
 from abc import ABC, abstractmethod
 
-from yogo.data import YOGO_CLASS_ORDERING
+from yogo.data import YOGO_CLASS_ORDERING, PERCENT_2_PARASITES_PER_UL
 
 
 class CountCorrector:
@@ -59,7 +59,7 @@ class CountCorrector:
 
         See remoscope manuscript for full derivation
         """
-        return np.matmul(np.square(raw_counts), np.square(self.inv_cmatrix_std))    
+        return np.matmul(np.square(raw_counts), np.square(self.inv_cmatrix_std))
 
     def calc_parasitemia(self, corrected_counts: npt.NDArray) -> float:
         """
@@ -69,13 +69,45 @@ class CountCorrector:
         rbcs = np.sum(corrected_counts[self.rbc_ids])
         return 0 if rbcs == 0 else parasites / rbcs
 
-    def calc_parasitemia_rel_err(self, count_vars: npt.NDArray, parasites: float) -> float:
+    def calc_parasitemia_rel_err(self, corrected_counts: npt.NDArray, count_vars: npt.NDArray) -> float:
         """
         Return relative uncertainty of total parasitemia count
 
         See remoscope manuscript for full derivation
         """
+        parasites = np.sum(corrected_counts[self.parasite_ids])
         parasite_count_vars = count_vars[self.parasite_ids]
 
         # Compute error
         return np.inf if parasites == 0 else np.sqrt(np.sum(parasite_count_vars)) / parasites
+
+    def get_all_res(
+        self, raw_counts: npt.NDArray, units_ul: bool=False 
+    ) -> Tuple[float, float, npt.NDArray]:
+        """
+        Return parasitemia, 95% confidence bound, and deskewed counts
+        See remoscope manuscript for full derivation
+
+        95% confidence interval can be defined as
+            lower_bound = max(0, parasitemia - bound)
+            upper_bound = min(1, parasitemia + bound)
+        """
+        # Deskew
+        corrected_counts = self.correct_counts(raw_counts)
+
+        # Calc_parasitemia
+        parasitemia = calc_parasitemia(corrected_counts)
+        
+        # Get uncertainties
+        count_vars = self.calc_count_vars(raw_counts)
+
+        # Use rule of 3 if there are no parasites
+        if parasitemia == 0:
+            bound = 3 / corrected_counts[YOGO_CLASS_IDX_MAP["healthy"]]
+        else:
+            bound = 1.69 * self.calc_parasitemia_rel_err(corrected_counts, count_vars)
+
+        if units_ul:
+            return parasitemia * PERCENT_2_PARASITES_PER_UL, bound * PERCENT_2_PARASITES_PER_UL, corrected_counts
+        else:
+            return parasitemia, bound, corrected_counts
