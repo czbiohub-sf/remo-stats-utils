4f3576aeb223b64a6c513d5843a3dfffe344ccd4
Michelle Khoo <michelle.khoo.wl@gmail.com>
2024-03-06 15:52:19 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ data_files = get_data_files("stats_utils/data_files")
 
 setup(
     name="stats_utils",
-    version="0.0.5",
+    version="0.0.6",
     description="Statistics utilities for remoscope and corresponding paper",
     long_description=readme(),
     url="https://github.com/czbiohub-sf/remo-stats-utils",
