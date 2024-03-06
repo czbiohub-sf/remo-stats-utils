eb6c7bd86ec319375afb833ee6722f3c0826f3c7
Michelle Khoo <michelle.khoo.wl@gmail.com>
2024-03-06 11:08:41 -0800

    Cleanup and use abs path

--- setup.py
+++ setup.py
@@ -9,33 +9,24 @@ Created on Oct 1, 2021
 from setuptools import setup, find_packages
 from pathlib import Path
 
-import os
 
 def readme():
     with open("README.md") as f:
         return f.read()
 
 def get_data_files(parent_dir):
-    paths = []
-    for (path, directories, filenames) in os.walk(parent_dir):
-        for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
-    return paths
-    # parent_path = Path(parent_dir)
-    # child_paths = [path for path in parent_path.iterdir() if path.is_dir()]
+    parent_path = Path(parent_dir)
+    child_paths = [path for path in parent_path.iterdir() if path.is_dir()]
 
-    # all_files = []
-    # for child_path in child_paths:
-    #     files = [path.as_posix() for path in child_path.iterdir() if path.is_file()]
-    #     all_files.extend(files)
+    all_files = []
+    for child_path in child_paths:
+        files = [path.resolve().as_posix() for path in child_path.iterdir() if path.is_file()]
+        all_files.extend(files)
 
-    # return all_files
+    return all_files
 
 
-# data_files = get_data_files('stats_utils/data_files')
-data_files = get_data_files('stats_utils/data_files/frightful-wendigo-1931')
-curr_dir = Path(__file__).parent.resolve()
-print(curr_dir)
+data_files = get_data_files('stats_utils/data_files')
 
 setup(
     name="stats_utils",
@@ -47,8 +38,7 @@ setup(
     author_email="michelle.khoo@czbiohub.org",
     license="MIT",
     packages=find_packages(),
-    # package_data={'stats_utils': data_files},
-    package_data={'stats_utils': ['../stats_utils/data_files/*']},
+    package_data={'stats_utils': data_files},
     include_package_data=True,
     install_requires=[
         "numpy",
